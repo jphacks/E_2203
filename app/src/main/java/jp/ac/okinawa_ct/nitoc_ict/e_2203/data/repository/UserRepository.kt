e6e624c8c8ea695eb6a0e8104d9041e44bbcea5b
hundo303 <sakaigen303@gmail.com>
2022-10-19 02:40:24 +0900

    UserRepository の updateUserId の引数が何も無かったので id をもらうように修正しました

--- app/src/main/java/jp/ac/okinawa_ct/nitoc_ict/e_2203/data/repository/UserRepository.kt
+++ app/src/main/java/jp/ac/okinawa_ct/nitoc_ict/e_2203/data/repository/UserRepository.kt
@@ -3,5 +3,5 @@ package jp.ac.okinawa_ct.nitoc_ict.e_2203.data.repository
 interface UserRepository {
     suspend fun getUserId(): Result<String>
 
-    suspend fun updateUserId(): Result<String>
+    suspend fun updateUserId(id: String): Result<String>
 }
