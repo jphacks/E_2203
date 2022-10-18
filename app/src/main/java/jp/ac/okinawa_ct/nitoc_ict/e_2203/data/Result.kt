b715b999bf6900ce11a052d504f48719a3f89945
hundo303 <sakaigen303@gmail.com>
2022-10-19 02:32:00 +0900

    Result 内の OK のプロパティ名を変更しました

--- app/src/main/java/jp/ac/okinawa_ct/nitoc_ict/e_2203/data/Result.kt
+++ app/src/main/java/jp/ac/okinawa_ct/nitoc_ict/e_2203/data/Result.kt
@@ -1,9 +1,15 @@
 package jp.ac.okinawa_ct.nitoc_ict.e_2203.data
 
+/**
+ * 失敗する可能性のある処理の結果を表現するクラス
+ */
 sealed class Result<out R> {
 
-    data class OK<out T>(val response: T): Result<T>()
+    data class OK<out T>(val data: T): Result<T>()
 
+    /**
+     * 処理が失敗したことを表現するクラス
+     */
     data class Error(
         val exception: java.lang.Exception,
     ): Result<Nothing>()
