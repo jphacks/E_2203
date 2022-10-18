2f4542e7c994af7e633ec4a9f5d1a04faadda857
hundo303 <sakaigen303@gmail.com>
2022-10-19 02:16:54 +0900

    Userのデータを扱う UserRepository を作成しました。

---
+++ app/src/main/java/jp/ac/okinawa_ct/nitoc_ict/e_2203/data/repository/UserRepository.kt
@@ -0,0 +1,7 @@
+package jp.ac.okinawa_ct.nitoc_ict.e_2203.data.repository
+
+interface UserRepository {
+    suspend fun getUserId(): Result<String>
+
+    suspend fun updateUserId(): Result<String>
+}
