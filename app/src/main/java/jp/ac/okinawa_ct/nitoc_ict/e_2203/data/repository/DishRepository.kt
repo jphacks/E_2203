e768d0911bc0381a9c1999f0a3f2fcb038efd899
hundo303 <sakaigen303@gmail.com>
2022-10-19 02:20:59 +0900

    DishRepository で UserIDを使うようにしました。

--- app/src/main/java/jp/ac/okinawa_ct/nitoc_ict/e_2203/data/repository/DishRepository.kt
+++ app/src/main/java/jp/ac/okinawa_ct/nitoc_ict/e_2203/data/repository/DishRepository.kt
@@ -4,9 +4,9 @@ import jp.ac.okinawa_ct.nitoc_ict.e_2203.data.Dish
 import jp.ac.okinawa_ct.nitoc_ict.e_2203.data.Situation
 
 interface DishRepository {
-    suspend fun getRecommendedDishList(situation: Situation): Result<List<Dish>>
+    suspend fun getRecommendedDishList(id: String, situation: Situation): Result<List<Dish>>
 
     suspend fun getDishList(): Result<List<Dish>>
 
-    suspend fun sendAteDish(dish: Dish, situation: Situation): Result<Nothing>
+    suspend fun sendAteDish(id: String, situation: Situation, dish: Dish): Result<Nothing>
 }
