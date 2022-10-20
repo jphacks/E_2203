c24935abda2229479ddc264c42c3b07ecc02a24d
NTokinawa <mi201319@edu.okinawa-ct.ac.jp>
2022-10-21 01:18:41 +0900

    SubActivityへの変遷の実装

--- app/src/main/java/jp/ac/okinawa_ct/nitoc_ict/e_2203/ui/MainActivity.kt
+++ app/src/main/java/jp/ac/okinawa_ct/nitoc_ict/e_2203/ui/MainActivity.kt
@@ -51,6 +51,9 @@ class MainActivity : AppCompatActivity() { //MainActivityクラス
         val more_button = findViewById<Button>(R.id.more_button)
         more_button.setOnClickListener{
             more_button.text="今押しただろ、お前（半ギレ）" //クリックイベント確認用
+            val intent = Intent(this, SubActivity::class.java) //第２引数に移動したいActivity
+            startActivity(intent)
+
             //画面変遷の処理を記述
         }
 
