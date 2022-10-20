8e410508502a8b38f1aa212412a3910ec0b06c03
NTokinawa <mi201319@edu.okinawa-ct.ac.jp>
2022-10-20 16:21:59 +0900

    UIのDish型への対応を実装

--- app/src/main/java/jp/ac/okinawa_ct/nitoc_ict/e_2203/UI/MainActivity.kt
+++ app/src/main/java/jp/ac/okinawa_ct/nitoc_ict/e_2203/UI/MainActivity.kt
@@ -7,6 +7,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import jp.ac.okinawa_ct.nitoc_ict.e_2203.R
+import jp.ac.okinawa_ct.nitoc_ict.e_2203.data.Dish
 
 class MainActivity : AppCompatActivity() { //MainActivityクラス
 
@@ -14,21 +15,26 @@ class MainActivity : AppCompatActivity() { //MainActivityクラス
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //テスト用↓ーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー
+        //テスト用↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓
+
         val testval1 = findViewById<TextView>(R.id.item1_text)
         val testval2 = findViewById<Button>(R.id.item1_button1)
         val testval3 = findViewById<Button>(R.id.item1_button2)
-        setItemText(testval1, "野獣の照り焼き")
-        setItemButton(testval2, "https://www.google.com/maps?client=firefox-b-d&q=%E6%9D%B1%E4%BA%AC%E9%83%BD%E4%B8%96%E7%94%B0%E8%B0%B7%E5%8C%BA%E5%8C%97%E6%B2%A2%EF%BC%93%E4%B8%81%E7%9B%AE%EF%BC%92%EF%BC%93%E2%88%92%EF%BC%91%EF%BC%94&um=1&ie=UTF-8&sa=X&ved=2ahUKEwjUwrzAke76AhVQY94KHTWkDpUQ_AUoAXoECAIQAw")
-        setItemButton(testval3, "https://cookpad.com/dining/recipes/2762072")
+        val testval4_yjloc = "https://www.google.com/maps?client=firefox-b-d&q=%E6%9D%B1%E4%BA%AC%E9%83%BD%E4%B8%96%E7%94%B0%E8%B0%B7%E5%8C%BA%E5%8C%97%E6%B2%A2%EF%BC%93%E4%B8%81%E7%9B%AE%EF%BC%92%EF%BC%93%E2%88%92%EF%BC%91%EF%BC%94&um=1&ie=UTF-8&sa=X&ved=2ahUKEwjUwrzAke76AhVQY94KHTWkDpUQ_AUoAXoECAIQAw"
+        val testval5_ohagi = "https://cookpad.com/dining/recipes/2762072"
+        val testval6_yjname = "野獣の照り焼き"
+        val testval7_yjdish = Dish(testval6_yjname, testval5_ohagi, testval4_yjloc)
 
-        //テスト用↑ーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー
+        setItemRoot(testval1, testval2, testval3, testval7_yjdish)
+
+        //テスト用↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑
 
     }
 
     //こ↑こ↓から書くーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー
 
-    //Dishのメンバ（String）とViewの参照（インスタンス）を受け取り、反映させる関数2つーーーーーーー
+
+    //Dishのメンバ（String）とViewの参照（インスタンス）を受け取り、反映させる関数2つ↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓
 
     fun setItemText(myTextView: TextView, name: String) { //Item*_text用
         myTextView.text = name
@@ -41,4 +47,22 @@ class MainActivity : AppCompatActivity() { //MainActivityクラス
             startActivity(intent)
         }
     }
+
+    //Dishのメンバ（String）とViewの参照（インスタンス）を受け取り、反映させる関数2つ↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑
+
+
+    //Dish型と、複数のインスタンスを受け取って、上の各表示関数に渡す関数↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓
+
+    fun setItemRoot(myText: TextView, myButtonLeft: Button, myButtonRight: Button, myDish: Dish) {
+        setItemText(myText, myDish.name)
+        setItemButton(myButtonLeft, myDish.mapUrl)
+        setItemButton(myButtonRight, myDish.recipeUrl)
+    }
+
+    //Dish型と、複数のインスタンスを受け取って、上の各表示関数に渡す関数↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑
+
+
 }
+
+//コメント用コピペ↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓
+//↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑
