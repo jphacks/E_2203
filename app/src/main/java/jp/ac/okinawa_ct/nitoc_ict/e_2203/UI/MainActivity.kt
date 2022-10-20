b10a57c6ec2aef6b12d4cb78d838ee60dc31b5a3
NTokinawa <mi201319@edu.okinawa-ct.ac.jp>
2022-10-20 15:42:13 +0900

    リスト用関数の一部を実装

--- app/src/main/java/jp/ac/okinawa_ct/nitoc_ict/e_2203/UI/MainActivity.kt
+++ app/src/main/java/jp/ac/okinawa_ct/nitoc_ict/e_2203/UI/MainActivity.kt
@@ -1,12 +1,44 @@
 package jp.ac.okinawa_ct.nitoc_ict.e_2203.UI
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import jp.ac.okinawa_ct.nitoc_ict.e_2203.R
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+class MainActivity : AppCompatActivity() { //MainActivityクラス
+
+    override fun onCreate(savedInstanceState: Bundle?) { //アプリ起動時の処理
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //テスト用↓ーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー
+        val testval1 = findViewById<TextView>(R.id.item1_text)
+        val testval2 = findViewById<Button>(R.id.item1_button1)
+        val testval3 = findViewById<Button>(R.id.item1_button2)
+        setItemText(testval1, "野獣の照り焼き")
+        setItemButton(testval2, "https://www.google.com/maps?client=firefox-b-d&q=%E6%9D%B1%E4%BA%AC%E9%83%BD%E4%B8%96%E7%94%B0%E8%B0%B7%E5%8C%BA%E5%8C%97%E6%B2%A2%EF%BC%93%E4%B8%81%E7%9B%AE%EF%BC%92%EF%BC%93%E2%88%92%EF%BC%91%EF%BC%94&um=1&ie=UTF-8&sa=X&ved=2ahUKEwjUwrzAke76AhVQY94KHTWkDpUQ_AUoAXoECAIQAw")
+        setItemButton(testval3, "https://cookpad.com/dining/recipes/2762072")
+
+        //テスト用↑ーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー
+
+    }
+
+    //こ↑こ↓から書くーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー
+
+    //Dishのメンバ（String）とViewの参照（インスタンス）を受け取り、反映させる関数2つーーーーーーー
+
+    fun setItemText(myTextView: TextView, name: String) { //Item*_text用
+        myTextView.text = name
+    }
+
+    fun setItemButton(myButton: Button, mapURL: String) { //Item*_button1とItem*_button2用
+        myButton.setOnClickListener{
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.data = Uri.parse(mapURL)
+            startActivity(intent)
+        }
     }
 }
